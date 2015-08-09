74222f76318d564555ee6cad2019a9999896d33b
Kevin Johnson <johnson786.kevin+git@gmail.com>
2015-08-09 21:23:02 +0000

    Implement exercises of chap 7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -11,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#qerror_explanation'
+    assert_select 'div.qfield_with_errors'
   end
   
   test "valid signup information" do
@@ -22,5 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
